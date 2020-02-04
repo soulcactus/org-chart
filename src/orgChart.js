299950653b0958caa4f06045afe6bbd24286d242
soulcactus <sunhyejin90@naver.com>
2020-02-04 16:58:18 +0900

    💎 Feat : Add profile feature

--- src/orgChart.js
+++ src/orgChart.js
@@ -1,35 +1,76 @@
 class OrgChart {
     constructor(options) {
-        const opts = JSON.parse(JSON.stringify(options));
-        const container = document.querySelector(opts['container']);
-        const data = opts['data'].sort((a, b) => a['id'] - b['id']);
+        const initialData = {
+            id: 0,
+            name: 'name',
+            parentId: null,
+        };
+
+        const initialProfile = {
+            name: 'name',
+        };
+
+        let opts;
+        let container;
+        let data;
         let tree;
 
-        data.forEach((item, index) => {
-            if (
-                !item.hasOwnProperty('id') ||
-                !item.hasOwnProperty('name') ||
-                !item.hasOwnProperty('parentId')
-            ) {
-                data.splice(index, 1);
-            }
+        opts = JSON.parse(JSON.stringify(options));
 
-            if (
-                index !== data.length - 1 &&
-                data[index]['id'] === data[index + 1]['id']
-            ) {
-                data.splice(index, 1);
-            }
+        if (!opts['data']) {
+            data = [initialData];
+        } else {
+            data = opts['data'].sort((a, b) => a['id'] - b['id']);
 
-            if (
-                item['parentId'] !== null &&
-                !data.some((val) => item['parentId'] === val['id'])
-            ) {
-                data.splice(index, 1);
+            data.forEach((item, index) => {
+                if (
+                    !item.hasOwnProperty('id') ||
+                    !item.hasOwnProperty('parentId') ||
+                    typeof item['id'] !== 'number' ||
+                    typeof item['parentId'] !== 'number' ||
+                    Number.isNaN(item['id']) ||
+                    Number.isNaN(item['parentId'])
+                ) {
+                    data.splice(index, 1);
+                }
+
+                if (
+                    index !== data.length - 1 &&
+                    data[index]['id'] === data[index + 1]['id']
+                ) {
+                    data.splice(index, 1);
+                }
+
+                if (
+                    item['parentId'] !== null &&
+                    !data.some((val) => item['parentId'] === val['id'])
+                ) {
+                    data.splice(index, 1);
+                }
+            });
+
+            if (!data.length) {
+                data = [initialData];
             }
-        });
+        }
+
+        container = document.querySelector(opts['container']);
+
+        if (!container) {
+            return console.error(
+                "[OrgChart] Error: 'container' property is null. 'container' property of the OrgChart option must be set.",
+            );
+        }
+
+        if (data.length !== options['data'].length) {
+            console.warn(
+                "[OrgChart] Warning: Some data items have invalid values or do not have required properties. They were excluded. You'd better check the data.",
+            );
+        }
 
         tree = this._treeModel(data);
+        this.usePhoto = opts['usePhoto'];
+        this.profile = opts['profile'] || initialProfile;
         this._printTree(tree, container);
         this.container = container;
         this.data = data;
@@ -43,11 +84,18 @@ class OrgChart {
         const container = this.container;
         const data = this.data;
         const id = ++this.id;
-        const name = 'test';
         const parentId = Number(e.target.parentNode.getAttribute('data-value'));
         let tree = this.tree;
+        let obj = {};
 
-        data.push({ id: id, name: name, parentId: parentId });
+        obj['id'] = id;
+        obj['parentId'] = parentId;
+
+        for (const [key, value] of Object.entries(this.profile)) {
+            obj[key] = value;
+        }
+
+        data.push(obj);
         tree = this._treeModel(data);
         container.innerHTML = '';
         this.tree = tree;
@@ -148,7 +196,10 @@ class OrgChart {
             const lineBox = document.createElement('div');
             const lineLeft = document.createElement('div');
             const lineRight = document.createElement('div');
+            const memberBox = document.createElement('div');
             const member = document.createElement('div');
+            const profileBox = document.createElement('div');
+            const profile = document.createElement('ul');
             const addBtn = document.createElement('button');
             const removeBtn = document.createElement('button');
 
@@ -169,15 +220,38 @@ class OrgChart {
                 }
             }
 
+            memberBox.className = 'member-box';
             member.className = 'member';
-            member.textContent = item['name'];
             member.setAttribute('data-value', item['id']);
+            profileBox.className = 'profile-box';
+            profile.className = 'profile';
             addBtn.className = 'add-btn';
-            addBtn.textContent = '추가';
+            addBtn.innerHTML = '&#43;';
             removeBtn.className = 'remove-btn';
-            removeBtn.textContent = '삭제';
+            removeBtn.innerHTML = '&#45;';
+
+            for (const [key, value] of Object.entries(this.profile)) {
+                const list = document.createElement('li');
+
+                if (this.usePhoto === true && key === 'photo') {
+                    const photo = document.createElement('div');
+                    const img = document.createElement('img');
+
+                    img.src = item[key] || value;
+                    photo.appendChild(img);
+                    profileBox.appendChild(photo);
+                } else {
+                    list.contentEditable = true;
+                    list.textContent = item[key] || value;
+                    profile.appendChild(list);
+                }
+            }
+
+            profileBox.appendChild(profile);
+            member.appendChild(profileBox);
             member.appendChild(addBtn);
             member.appendChild(removeBtn);
+            memberBox.appendChild(member);
             lineBox.appendChild(lineLeft);
             lineBox.appendChild(lineRight);
 
@@ -185,7 +259,7 @@ class OrgChart {
                 groupColumn.appendChild(lineBox);
             }
 
-            groupColumn.appendChild(member);
+            groupColumn.appendChild(memberBox);
 
             if (children) {
                 const lineBox = document.createElement('div');
@@ -235,6 +309,13 @@ class OrgChart {
 
 const orgChart = new OrgChart({
     container: '#container',
+    usePhoto: true,
+    profile: {
+        name: 'name',
+        tel: '000-0000-0000',
+        title: 'none',
+        photo: './images/profile.png',
+    },
     data: [
         {
             id: 0,
@@ -244,6 +325,8 @@ const orgChart = new OrgChart({
         {
             id: 0,
             name: 'administrator1',
+            tel: '010-1234-5678',
+            title: 'CEO',
             parentId: null,
         },
         {
@@ -287,7 +370,7 @@ const orgChart = new OrgChart({
         },
         {
             id: 9,
-            name: 'child4',
+            // name: 'child4',
             parentId: 1,
         },
         {
@@ -304,3 +387,5 @@ const orgChart = new OrgChart({
 });
 
 orgChart.print();
+
+console.log('??');
