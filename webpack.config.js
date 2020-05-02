b532d09fee610432f588212a1e55aa90975234eb
soulcactus <sunhyejin90@naver.com>
2020-05-02 16:15:36 +0900

    🔧 Fix : Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,6 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
             },
-            {
-                test: /\.css$/,
-                use: 'css-loader',
-            },
         ],
     },
 };
